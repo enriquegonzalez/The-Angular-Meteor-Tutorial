85b8193e4f7eb36e55527371fb34d3d49bd7acb8
Enrique <egonzalez78@gmail.com>
2015-03-25 19:06:25 -0700

    working on step 08, and added my own scope watch to set the save button label

--- client/parties/controllers/partyDetails.js
+++ client/parties/controllers/partyDetails.js
@@ -1,13 +1,23 @@
 angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteor',
 function($scope, $stateParams, $meteor){
 
+  $scope.stateMessage = "save";
   $scope.party = $meteor.object(Parties, $stateParams.partyId, false);
+
+  $scope.$watchGroup(['party.name', 'party.description'], function(newVal, oldVal) {
+    if(!newVal) return;
+    console.log(newVal, oldVal);
+      return $scope.stateMessage = "save";
+  });
+
 
    $scope.save = function() {
     $scope.party.save().then(function(numberOfDocs){
-      console.log('save success doc affected ', numberOfDocs);
+      $scope.stateMessage = "saved!";
+      // console.log('save success doc affected ', numberOfDocs);
     }, function(error){
-      console.log('save error', error);
+      $scope.stateMessage = "error";
+      // console.log('save error', error);
     });
   };
 
