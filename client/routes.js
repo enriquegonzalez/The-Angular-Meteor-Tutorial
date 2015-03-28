cca237ca501e9b601d358655f26872f8e683740a
Enrique <egonzalez78@gmail.com>
2015-03-28 16:58:12 -0700

    added run block for auth error redirects

--- client/routes.js
+++ client/routes.js
@@ -1,3 +1,14 @@
+angular.module("socially")
+.run(["$rootScope", "$location", function($rootScope, $location) {
+  $rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
+    // We can catch the error thrown when the $requireUser promise is rejected
+    // and redirect the user back to the main page
+    if (error === "AUTH_REQUIRED") {
+      $location.path("/parties");
+    }
+  });
+}]);
+
 angular.module('socially')
   .config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
     function($urlRouterProvider, $stateProvider, $locationProvider){
