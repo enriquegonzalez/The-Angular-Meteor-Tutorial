c01ae3a12cd0cdb8c59b1b56118b5956c8cf795e
Enrique <egonzalez78@gmail.com>
2015-03-29 12:09:44 -0700

    parties only show if owner is current user or they are public

--- server/startup/loadParties.js
+++ server/startup/loadParties.js
@@ -4,10 +4,12 @@ Meteor.startup(function () {
     var parties = [
       {'name': 'Popeye Party',
         'description': 'Spinach, Olives, Tobacco, and more. . .',
-        'owner': '6KHtWinP9FWSzvefA'},
+        'owner': '6KHtWinP9FWSzvefA',
+        'public': true},
       {'name': 'All DUBSTEP',
         'description': 'wowowowooooooomp WOMPWOMPWAAAAAOOAOAOAOAOMP chirp!',
-        'owner': '6KHtWinP9FWSzvefA'}
+        'owner': '6KHtWinP9FWSzvefA',
+        'public': true}
     ];
 
     for (var i = 0; i < parties.length; i++) {
